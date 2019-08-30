6b1457071bcb698e9e64f051ec340f599d64e086
Chaynor Hsiao <chaynsaw@gmail.com>
2019-08-30 15:52:16 -0700

    attempt tree From Array

---
+++ CS Prompts/constructTreeFromArray.js
@@ -0,0 +1,20 @@
+function Tree(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
+let tree = new Tree(3);
+let arr = [3,6,2,9,-1,10]
+function arrayToTree(arr) {
+  let newTree = new Tree(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (newTree.left == null) {
+
+    }
+  }
+}
+
+Tree.prototype.setNode = function() {
+  
+}
+
